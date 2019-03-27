92fad720c4c105400a808fa0ff17ebd93940a697
Taras Malynovskyi <tarasmalynovskyy@gmail.com>
2019-03-27 21:38:45 +0200

    HW#4 not finished, need one more task

---
+++ HW#4/src/task2/SumFirstFive.java
@@ -0,0 +1,36 @@
+package task2;
+import java.util.Arrays;
+
+public class SumFirstFive {
+
+	public static void main(String[] args) {
+
+		int[] tenNum = {-1, 222, 3, 4, 5, 1, 2, 31, 4, 5 };
+		int[] newArray = Arrays.copyOfRange(tenNum, 5, tenNum.length);
+		int[] newArray2 = Arrays.copyOfRange(tenNum, 0, 6);
+
+		int summ = 0;
+		int prod = 1;
+
+		for (int i = 0; i < newArray2.length; i++) {
+			if (newArray2[0] >= 0 && newArray2[1] >= 0 && newArray2[2] >= 0 && newArray2[3] >= 0 && newArray2[4] >= 0
+					&& i < 5) {
+				summ = summ + newArray2[i];
+
+			} else {
+				for (int j = 0; j < newArray.length; j++) {
+					prod = prod * newArray[j];
+				}
+				break;
+			}
+
+		}
+		if (summ > 0) {
+			System.out.println("Summ of first 5 int: " + summ);
+		} else {
+			System.out.println("Product of last 5 int: " + prod);
+		}
+
+	}
+
+}
