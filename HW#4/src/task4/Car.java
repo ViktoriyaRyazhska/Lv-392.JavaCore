1915e0dd92228fe45da2940a4b25d6df80a13d95
Taras Malynovskyi <tmalytc@training.local>
2019-03-28 16:53:46 +0200

    Make Car class

---
+++ HW#4/src/task4/Car.java
@@ -0,0 +1,27 @@
+package task4;
+
+public class Car {
+
+	private String type;
+	private int year;
+	private int engCap;
+
+	public Car() {
+	}
+
+	public Car(String type, int engCap) {
+		this.type = type;
+		this.engCap = engCap;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	
+	
+}
