a8c07534e7da9dc47d133f67b801bee1e01449d1
Taras Malynovskyi <tarasmalynovskyy@gmail.com>
2019-03-28 21:59:43 +0200

    HW#4 done. Sorting array car by year production

--- HW#4/src/task4/Car.java
+++ HW#4/src/task4/Car.java
@@ -4,24 +4,35 @@ public class Car {
 
 	private String type;
 	private int year;
-	private int engCap;
-
+	private double engCap;
+	
 	public Car() {
 	}
 
-	public Car(String type, int engCap) {
+	public Car(String type, double engCap) {
 		this.type = type;
 		this.engCap = engCap;
 	}
 
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public int getYear() {
-		return year;
+		return this.year;
 	}
 
 	public void setYear(int year) {
 		this.year = year;
 	}
-	
-	
-	
-}
+
+	@Override
+	public String toString() {
+		return "Type=" + type + ", year=" + year + ", engCap=" + engCap;
+	}
+	  
+	}
