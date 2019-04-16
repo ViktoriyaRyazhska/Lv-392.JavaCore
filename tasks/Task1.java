2c345753357b2781280762c4f36b6e6395bc13a7
Kate <katia.detsyk@gmail.com>
2019-04-16 19:31:58 +0300

    add all

--- tasks/Task1.java
+++ tasks/Task1.java
@@ -10,16 +10,20 @@ public class Task1 {
         System.out.println("Please enter the number: ");
         int n = scan.nextInt();
 
-        System.out.println("Fibonacci numbers including " + n + " place:");
-        int prevFibonacciNumber = 0;
-        int thisFibonacciNumber = 1;
-        int nextFibonacciNumber;
-        System.out.print(thisFibonacciNumber + " ");
-        for (int i = 1; i < n; i++) {
-            nextFibonacciNumber = prevFibonacciNumber + thisFibonacciNumber;
-            prevFibonacciNumber = thisFibonacciNumber;
-            thisFibonacciNumber = nextFibonacciNumber;
-            System.out.print(nextFibonacciNumber + " ");
+        if (n > 0) {
+            System.out.println("Fibonacci numbers including " + n + " place:");
+            int prevFibonacciNumber = 0;
+            int thisFibonacciNumber = 1;
+            int nextFibonacciNumber;
+            System.out.print(thisFibonacciNumber + " ");
+            for (int i = 1; i < n; i++) {
+                nextFibonacciNumber = prevFibonacciNumber + thisFibonacciNumber;
+                prevFibonacciNumber = thisFibonacciNumber;
+                thisFibonacciNumber = nextFibonacciNumber;
+                System.out.print(nextFibonacciNumber + " ");
+            }
+        }else{
+            System.out.println("Number is not positiv.");
         }
     }
 }
